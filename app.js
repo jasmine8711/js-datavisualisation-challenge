b6838970bc324d1a140786da8102ca9f2d473da1
Jasmine Mao <jasmine8711@gmail.com>
2019-07-24 15:25:36 +0200

    second chart works

--- app.js
+++ app.js
@@ -1,22 +1,4 @@
-// const axios = require("axios");
-/*
-// Make a request for a user with a given ID
-axios
-  .get(" https://canvasjs.com/services/data/datapoints.php")
-  .then(function(response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function(error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function() {
-    // always executed
-    console.log("final");
-  });
-  */
-function creatChart(tableID, style, labelsArray, datasetsArray) {
+function creatChart(tableID, styles, labelsArray, datasetsArray) {
   const table1 = document.getElementById(`${tableID}`);
   const canvas1 = document.createElement("canvas");
   table1.parentElement.insertBefore(canvas1, table1);
@@ -25,7 +7,7 @@ function creatChart(tableID, style, labelsArray, datasetsArray) {
   //canvas1.style.border = "blue solid 1px";
   var ctx = canvas1.getContext("2d");
   var lineChart = new Chart(ctx, {
-    type: `${style}`,
+    type: styles,
     data: {
       labels: labelsArray,
       datasets: datasetsArray
@@ -41,19 +23,10 @@ function creatChart(tableID, style, labelsArray, datasetsArray) {
 }
 
 const years = [];
-
-//console.log(table1.rows[1]);
 //years
 for (var t = 0; t < table1.rows[1].cells.length - 2; t++) {
   m = t + 2;
   years[t] = table1.rows[1].cells[m].innerHTML.toLowerCase().replace(/ /gi, "");
-}
-//headers
-var headers = [];
-for (var i = 0; i < table1.rows[0].cells.length; i++) {
-  headers[i] = table1.rows[0].cells[i].innerHTML
-    .toLowerCase()
-    .replace(/ /gi, "");
 }
 
 // datapoints
@@ -95,29 +68,57 @@ const datasets = [];
 for (let i = 0; i < country.length; i++) {
   datasets.push(new Datasets(i));
 }
-console.log(datasets);
-/* var datasets = [
-    {
-      label: "beijing",
-      borderColor: "rgb(255, 99, 132)",
-      data: [0, 10, 5, 2, 20, 30, 45],
-      fill: false
-    },
-    {
-      label: "China",
-      borderColor: "rgb(11, 99, 132)",
-      data: [3, 14, 5, 2, 23, 32, 25],
-      fill: false
-    }
-  ];
-  var newDatesets = {
-    label: country[4],
-    borderColor: "rgb(11, 99, 132)",
-    data: [],
-    fill: false
-  };
-  console.log(newDatesets);
-  datasets.push(newDatesets);
-  */
+
+//-------------------------chart2
+const table2 = document.getElementById("table2");
+//country2
+const country2 = [];
+for (let i = 1; i < table2.rows.length; i++) {
+  country2.push(table2.rows[i].cells[1].innerHTML);
+}
+//console.log(country2);
+//date
+const date = [];
+for (var i = 2; i < table2.rows[0].cells.length; i++) {
+  date[i - 2] = table2.rows[0].cells[i].innerHTML
+    .toLowerCase()
+    .replace(/ /gi, "");
+}
+//console.log(date[0]);
+
+const datapoint1 = [];
+const datapoint2 = [];
+
+for (let i = 1; i < table2.rows.length; i++) {
+  datapoint1.push(table2.rows[i].cells[2].innerText);
+  datapoint2.push(table2.rows[i].cells[3].innerText);
+}
+const color = ["#A7D2D6", "#F5C1C7"];
+
+function BarSet(i, j) {
+  this.label = date[i];
+  this.data = j;
+  this.backgroundColor = color[i];
+  this.fill = true;
+}
+const datasets2 = [];
+datasets2.push(new BarSet(0, datapoint1));
+datasets2.push(new BarSet(1, datapoint2));
+//console.log(datasets2);
 
 creatChart("table1", "line", years, datasets);
+creatChart("table2", "bar", country2, datasets2);
+//-------------------------fetch data from json api
+const data3 = [
+  [0, 9],
+  [1, 6],
+  [2, 7],
+  [3, 7],
+  [4, 7],
+  [5, 8],
+  [6, 9],
+  [7, 5],
+  [8, 3],
+  [9, 8]
+];
+creatChart("firstHeading", "bubble", data3, datasets2);
